c2d2f1e12811e1ef087f3fa92f4a542111081f52
SupasanKomonlit <supasan_224@hotmail.com>
2019-12-24 00:08:55 +0700

    Test vector

---
+++ cpp_test/src/cpp_vector.cpp
@@ -0,0 +1,15 @@
+#include    <iostream>
+
+#include    <vector>
+
+int main( int argv , char** argc )
+{
+    std::vector temp_vector;
+    temp_vector.assign( 8 , 0 );
+    temp_vector.at( 5 ) = 10;
+    for( unsigned int run = 0 ; run < 8 ; run++ )
+    {
+        std::cout   << temp_vector.at( run ) << " " ;
+    }
+    std::cout   << "\n";
+}
